f175f15743b96e50dc2b0316a0aa7cc30205b398
Danijel Cvit <danijel+github@gmail.com>
2022-02-28 02:19:56 +0100

    player controls work for the most part

--- src/components/WebPlayback.jsx
+++ src/components/WebPlayback.jsx
@@ -1,4 +1,81 @@
 import React, { useState, useEffect, useRef } from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Slider from "@mui/material/Slider";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import PauseRounded from "@mui/icons-material/PauseRounded";
+import PlayArrowRounded from "@mui/icons-material/PlayArrowRounded";
+import FastForwardRounded from "@mui/icons-material/FastForwardRounded";
+import FastRewindRounded from "@mui/icons-material/FastRewindRounded";
+import VolumeUpRounded from "@mui/icons-material/VolumeUpRounded";
+import VolumeDownRounded from "@mui/icons-material/VolumeDownRounded";
+
+const WallPaper = styled("div")({
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  top: 0,
+  left: 0,
+  overflow: "hidden",
+  background: "linear-gradient(rgb(255, 38, 142) 0%, rgb(255, 105, 79) 100%)",
+  transition: "all 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s",
+  "&:before": {
+    content: '""',
+    width: "140%",
+    height: "140%",
+    position: "absolute",
+    top: "-40%",
+    right: "-50%",
+    background:
+      "radial-gradient(at center center, rgb(62, 79, 249) 0%, rgba(62, 79, 249, 0) 64%)",
+  },
+  "&:after": {
+    content: '""',
+    width: "140%",
+    height: "140%",
+    position: "absolute",
+    bottom: "-50%",
+    left: "-30%",
+    background:
+      "radial-gradient(at center center, rgb(247, 237, 225) 0%, rgba(247, 237, 225, 0) 70%)",
+    transform: "rotate(30deg)",
+  },
+});
+
+const Widget = styled("div")(({ theme }) => ({
+  padding: 16,
+  borderRadius: 16,
+  width: 343,
+  maxWidth: "100%",
+  margin: "auto",
+  position: "relative",
+  zIndex: 1,
+  backgroundColor:
+    theme.palette.mode === "dark" ? "rgba(0,0,0,0.6)" : "rgba(255,255,255,0.4)",
+  backdropFilter: "blur(40px)",
+}));
+
+const CoverImage = styled("div")({
+  width: 100,
+  height: 100,
+  objectFit: "cover",
+  overflow: "hidden",
+  flexShrink: 0,
+  borderRadius: 8,
+  backgroundColor: "rgba(0,0,0,0.08)",
+  "& > img": {
+    width: "100%",
+  },
+});
+
+const TinyText = styled(Typography)({
+  fontSize: "0.75rem",
+  opacity: 0.38,
+  fontWeight: 500,
+  letterSpacing: 0.2,
+});
 
 const track = {
   name: "",
@@ -6,20 +83,65 @@ const track = {
     images: [{ url: "" }],
   },
   artists: [{ name: "" }],
+  duration_ms: 0,
 };
 
 const WebPlayback = ({ token }) => {
+  const theme = useTheme();
+
+  function formatDuration(value) {
+    const minute = Math.floor(value / 60);
+    const secondLeft = value - minute * 60;
+    return `${minute}:${secondLeft <= 9 ? `0${secondLeft}` : secondLeft}`;
+  }
+  const mainIconColor = theme.palette.mode === "dark" ? "#fff" : "#000";
+  const lightIconColor =
+    theme.palette.mode === "dark" ? "rgba(255,255,255,0.4)" : "rgba(0,0,0,0.4)";
+
   const [player, setPlayer] = useState(undefined);
   const [is_paused, setPaused] = useState(false);
   const [is_active, setActive] = useState(false);
   const [current_track, setTrack] = useState(track);
+  const [volume, setVolume] = useState(30);
+  const [position, setPosition] = useState(0);
 
+  const duration = Math.floor(current_track.duration_ms / 1000); // seconds
+
+  // Keep new token value to pass to the player (auto requests new token after 1h)
   const newToken = useRef();
-
-  console.log(newToken);
-
   newToken.current = token;
 
+  const handleVolume = (_, newValue) => {
+    player.setVolume(newValue / 100);
+    setVolume(newValue);
+  };
+
+  const handleSeek = (_, newValue) => {
+    player.seek(newValue * 1000);
+    setPosition(newValue);
+  };
+
+  // Update the slider time indicator position every second
+
+  useEffect(() => {
+    setPosition(0);
+  }, [JSON.stringify(current_track)]);
+
+  useEffect(() => {
+    if (!is_active || is_paused) {
+      return;
+    }
+    const interval = setInterval(() => {
+      if (position >= duration) {
+        clearInterval(interval);
+      }
+      setPosition(position + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [position, is_paused, is_active]);
+
+  // Add the SDK to the DOM and initialize the player
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://sdk.scdn.co/spotify-player.js";
@@ -33,7 +155,7 @@ const WebPlayback = ({ token }) => {
         getOAuthToken: (cb) => {
           cb(newToken.current);
         },
-        volume: 0.5,
+        volume: volume / 100,
       });
 
       setPlayer(player);
@@ -82,51 +204,145 @@ const WebPlayback = ({ token }) => {
   }, []);
 
   return (
-    <>
-      <div className="container">
-        <div className="main-wrapper">
-          <img
-            src={current_track.album.images[0].url}
-            className="now-playing__cover"
-            alt=""
-          />
-
-          <div className="now-playing__side">
-            <div className="now-playing__name">{current_track.name}</div>
-            <div className="now-playing__artist">
+    <Box sx={{ width: "100%", overflow: "hidden" }}>
+      <Widget>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          {current_track.album.images[0].url && (
+            <CoverImage>
+              <img
+                alt="can't win - Chilling Sunday"
+                src={current_track.album.images[0].url}
+              />
+            </CoverImage>
+          )}
+          <Box sx={{ ml: 1.5, minWidth: 0 }}>
+            <Typography
+              variant="caption"
+              color="text.secondary"
+              fontWeight={500}
+            >
               {current_track.artists[0].name}
-            </div>
-
-            <button
-              className="btn-spotify"
-              onClick={() => {
-                player.previousTrack();
-              }}
-            >
-              &lt;&lt;
-            </button>
-
-            <button
-              className="btn-spotify"
-              onClick={() => {
-                player.togglePlay();
-              }}
-            >
-              {is_paused ? "PLAY" : "PAUSE"}
-            </button>
-
-            <button
-              className="btn-spotify"
-              onClick={() => {
-                player.nextTrack();
-              }}
-            >
-              &gt;&gt;
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
+            </Typography>
+            <Typography noWrap>
+              <b>{current_track.name}</b>
+            </Typography>
+          </Box>
+        </Box>
+        <Slider
+          aria-label="time-indicator"
+          size="small"
+          value={position}
+          min={0}
+          step={1}
+          max={duration}
+          onChange={handleSeek}
+          sx={{
+            color: theme.palette.mode === "dark" ? "#fff" : "rgba(0,0,0,0.87)",
+            height: 4,
+            "& .MuiSlider-thumb": {
+              width: 8,
+              height: 8,
+              transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
+              "&:before": {
+                boxShadow: "0 2px 12px 0 rgba(0,0,0,0.4)",
+              },
+              "&:hover, &.Mui-focusVisible": {
+                boxShadow: `0px 0px 0px 8px ${
+                  theme.palette.mode === "dark"
+                    ? "rgb(255 255 255 / 16%)"
+                    : "rgb(0 0 0 / 16%)"
+                }`,
+              },
+              "&.Mui-active": {
+                width: 20,
+                height: 20,
+              },
+            },
+            "& .MuiSlider-rail": {
+              opacity: 0.28,
+            },
+          }}
+        />
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            mt: -2,
+          }}
+        >
+          <TinyText>{formatDuration(position)}</TinyText>
+          <TinyText>{formatDuration(duration)}</TinyText>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            mt: -1,
+          }}
+        >
+          <IconButton
+            onClick={() => player.previousTrack()}
+            aria-label="previous song"
+          >
+            <FastRewindRounded fontSize="large" htmlColor={mainIconColor} />
+          </IconButton>
+          <IconButton
+            onClick={() => player.togglePlay()}
+            aria-label={is_paused ? "play" : "pause"}
+          >
+            {is_paused ? (
+              <PlayArrowRounded
+                sx={{ fontSize: "3rem" }}
+                htmlColor={mainIconColor}
+              />
+            ) : (
+              <PauseRounded
+                sx={{ fontSize: "3rem" }}
+                htmlColor={mainIconColor}
+              />
+            )}
+          </IconButton>
+          <IconButton onClick={() => player.nextTrack()} aria-label="next song">
+            <FastForwardRounded fontSize="large" htmlColor={mainIconColor} />
+          </IconButton>
+        </Box>
+        <Stack
+          spacing={2}
+          direction="row"
+          sx={{ mb: 1, px: 1 }}
+          alignItems="center"
+        >
+          <VolumeDownRounded htmlColor={lightIconColor} />
+          <Slider
+            aria-label="Volume"
+            value={volume}
+            onChange={handleVolume}
+            sx={{
+              color:
+                theme.palette.mode === "dark" ? "#fff" : "rgba(0,0,0,0.87)",
+              "& .MuiSlider-track": {
+                border: "none",
+              },
+              "& .MuiSlider-thumb": {
+                width: 24,
+                height: 24,
+                backgroundColor: "#fff",
+                "&:before": {
+                  boxShadow: "0 4px 8px rgba(0,0,0,0.4)",
+                },
+                "&:hover, &.Mui-focusVisible, &.Mui-active": {
+                  boxShadow: "none",
+                },
+              },
+            }}
+          />
+          <VolumeUpRounded htmlColor={lightIconColor} />
+        </Stack>
+      </Widget>
+      <WallPaper />
+    </Box>
   );
 };
 
